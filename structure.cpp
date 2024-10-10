1230ff401ed1ea147210160a0b942d5bcc54be91
sapunjgogoi <sapunjgogoi@gmail.com>
2024-10-10 22:36:55 +0530

    created the structure of cpp

---
+++ structure.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h> // header file ??check why we use
+using namespace std; // for standard input output
+
+int main() {
+    cout<<"Hello World!";
+    return 0;
+}
+
+// simple structure that prints "Hello World!"
